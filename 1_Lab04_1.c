c3a75be5cedfd7c7450ccb5f846702ac3c7ef598
ooyoo80 <ooyoo80@gmail.com>
2025-05-01 12:52:54 +0900

    1_Lab04

---
+++ 1_Lab04_1.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+void add();
+
+int main() 
+{
+	int a, b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	add(a, b);
+	return 0;
+}
+void add(int a, int b) 
+{
+	printf("%d", a+b);
+}
---
+++ 1_Lab04_2.c
@@ -0,0 +1,20 @@
+/*#include <stdio.h>
+
+void add();
+
+int main()
+{
+	int a, b, count;
+	scanf("%d", &count);
+	while (count > 0) {
+		scanf("%d %d", &a, &b);
+		add(a, b);
+		count -= 1;
+	}
+
+	return 0;
+}
+void add(int a, int b)
+{
+	printf("%d\n", a + b);
+}*/
