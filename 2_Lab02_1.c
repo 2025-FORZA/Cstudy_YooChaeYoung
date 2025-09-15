02c1a9f812b93486e88df61d38e1e1c7b29d0443
ooyoo80 <ooyoo80@gmail.com>
2025-09-15 16:21:51 +0900

    2_Lab02

---
+++ 2_Lab02_1.c
@@ -0,0 +1,17 @@
+﻿/*#include <stdio.h>
+int main() {
+	int year;
+	scanf("%d", &year);
+
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		printf("1\n");
+	}
+	else {
+		printf("0\n");
+	}
+
+	return 0;
+}*/
+
+/*윤년:(연도가 4의 배수 -> 100의 배수 X) or 400의 배수
+윤년이면 1, 아니면 0*/
---
+++ 2_Lab02_2.c
@@ -0,0 +1,12 @@
+﻿/*#include <stdio.h>
+int main() {
+	long long n;
+	long long add = 0;
+	scanf("%lld", &n);
+
+	for (long long i = 1; i <= n; i++) {
+		add += i;
+	}
+	printf("%lld\n", add);
+	return 0;
+}*/
